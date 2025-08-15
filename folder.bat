aab9d6b5e498cc5052889165a3f7ee3691b220b4
User <0008.maulanaa@gmail.com>
2025-08-15 16:49:38 +0700

    Update WiFi Data Fri 08/15/2025 16:49:38.55

--- folder.bat
+++ folder.bat
@@ -1,9 +1,17 @@
 @echo off
-cd /d "D:\Project\WiFiWord"
+setlocal
 
+:: Pindah ke lokasi file .bat ini berada
+cd /d "%~dp0"
+
+:: Commit & push otomatis
 git add .
-git commit -m "Update data WiFi %date% %time%"
+git commit -m "Update WiFi Data %date% %time%"
+git pull origin main
 git push origin main
 
+:: Popup sukses
 mshta "javascript:alert('✅ Data WiFi berhasil diupload ke GitHub!');close()"
+
+endlocal
 exit
